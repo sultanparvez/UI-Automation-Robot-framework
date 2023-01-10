c32a92929bb1c7dd2e4dd063aff2648102e3a902
sultanparvez <sultanparvez77.sp@gmail.com>
2023-01-10 14:13:20 +0600

    cleanup

--- customLibraries/Shop.py
+++ customLibraries/Shop.py
@@ -7,10 +7,7 @@ class Shop:
 
     def __init__(self):
         self.selLib = BuiltIn().get_library_instance("SeleniumLibrary")
-
-    # @keyword
-    # def hello_world(self):
-    #     print("hello")
+)
 
     @keyword
     def add_items_to_cart_and_checkout(self, productsList):
@@ -19,7 +16,7 @@ class Shop:
         productsTitles = self.selLib.get_webelements(" css:.card-title")
         for productsTitle in productsTitles:
             if productsTitle.text in productsList:
-                self.selLib.click_button("xpath:(//*[@class='card-footer'])["+str(i)+"]/button")
+                sl.click_button("xpath:(//*[@class='card-footer'])["+str(i)+"]/button")
 
             i = i + 1
 
